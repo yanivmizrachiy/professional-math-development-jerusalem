671b7ddf5284005a33dfa5863a07041e65b6e1f6
yanivmizrachiy <yanivmiz77@gmail.com>
2025-11-27 22:11:54 +0200

    fix: correct GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
+export default defineConfig({ base: '/professional-math-development-jerusalem/', ({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       server: {
